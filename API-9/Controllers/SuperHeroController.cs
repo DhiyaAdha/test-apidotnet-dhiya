4d7ae8e4f9346787a5fe89535c0131ee957bf7a6
Dhiya <adiadiadha@google.com>
2023-09-19 11:59:24 +0700

    up dhiya

---
+++ API-9/Controllers/SuperHeroController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "dhiya",
+                    FirstName = "dha",
+                    LastName = "hadi",
+                    Place = "jakarta",
+                    Email = "adiadi@gmail.com"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "faiz",
+                    FirstName = "fa",
+                    LastName = "iz",
+                    Place = "jakarta",
+                    Email = "faiz@gmail.com"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Hero Not Found");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("hero not found");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+            hero.Email = request.Email;
+            
+            return Ok(heroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("hero not found");
+
+            heroes.Remove(hero);
+            return Ok(hero);
+
+        }
+    }
+}
